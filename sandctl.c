4e9b413fe199fb9f4ddd55a4e09b5c906a0b4bc1
mihmiha555 <mih_miha555@mail.ru>
2022-08-14 23:16:23 +0300

    Rework sandctl utility
    Original sandctl utility is inconvenient to use, particularly
    if the guest code is quite large. Therefore, it was decided
    to add the ability to load guest code from file. The file
    must be in BFD "binary" format. The new mode is activated
    with "-f" flag. However, the legacy mode is retained for
    compatibility reasons.
    
    Bug fixes:
    -- #4: Possibility of code buffer overflow.
    -- #5: Closing non-valid file descriptor (EBADF).

--- sandctl.c
+++ sandctl.c
@@ -7,6 +7,8 @@
 #include <errno.h>
 #include <fcntl.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
 
@@ -45,47 +47,112 @@ int main(int argc, char *argv[])
 	uint8_t code[MAX_CODE_LEN];
 	int fd;
 	int arg;
+	int size;
 
 	if (argc == 1) {
-		printf("Use: %s < space-separated hex bytes >\n", argv[0]);
+		printf("Usage: %s -f < binary file >\n", argv[0]);
+		printf("Example: %s -f code.bin\n", argv[0]);
+		printf("Legacy usage: %s < space-separated hex bytes >\n", argv[0]);
 		printf("Example: %s 0xCC 0xCD 0x10 0xF4\n", argv[0]);
 		return 0;
 	}
 
-	for (arg = 1; arg < argc; arg++) {
-		unsigned long hex = 0;
-		char *endptr;
+	if (!strcmp(argv[1], "-f")) {	// New mode
+		int fd_in;
+		struct stat sb;
+		size_t len;
+		ssize_t ret, nr = 0;
 
-		hex = strtoul(argv[arg], &endptr, 16);
-
-		if (*endptr != '\0') {
-			fprintf(stderr, "Failed to parse code at pos %d.\n", arg - 1);
-			return 1;
+		if(argc == 2) {
+			fprintf(stderr, "Please, specify the binary code file.\n");
+			return 5;
 		}
 
-		if (hex > 0xFFul) {
-			fprintf(stderr, "Too large code val at pos %d.\n", arg - 1);
-			fprintf(stderr, "Every val must be a byte.\n");
-			return 2;
+		fd_in = open(argv[2], O_RDONLY);
+		if (fd_in < 0) {
+			fprintf(stderr, "%s: Failed to open binary file.\n", argv[2]);
+			return 6;
 		}
 
-		code[arg - 1] = (uint8_t)hex;
+		if(fstat(fd_in, &sb)) {
+			fprintf(stderr, "%s: Failed to get stat of the file.\n", argv[2]);
+			close(fd_in);
+			return 7;
+		}
+
+		len = (size_t)sb.st_size;
+		if(!len) {
+			fprintf(stderr, "%s: Binary file is empty\n", argv[2]);
+			close(fd_in);
+			return 8;
+		}
+
+		if(len > MAX_CODE_LEN) {
+			fprintf(stderr, "%s: Binary file exceeds the allowed size (%d bytes).\n", argv[2], MAX_CODE_LEN);
+			close(fd_in);
+			return 9;
+		}
+
+		size = (int)len;
+
+		while (len != 0 && (ret = read(fd_in, &code[nr], len)) != 0) {
+			if (ret == -1) {
+				if (errno == EINTR)
+					continue;
+
+				fprintf(stderr, "%s: Failed to read code from file.\n", argv[2]);
+				close(fd_in);
+				return 10;
+			}
+
+			len -= ret;
+			nr += ret;
+		}
+		close(fd_in);
+
+	} else {	// Legacy mode
+
+		size = argc - 1;
+
+		if(size > MAX_CODE_LEN) {
+			fprintf(stderr, "Code exceeds the allowed size (%d bytes).\n", MAX_CODE_LEN);
+			return 9;
+		}
+
+		for (arg = 1; arg < argc; arg++) {
+			unsigned long hex = 0;
+			char *endptr;
+
+			hex = strtoul(argv[arg], &endptr, 16);
+
+			if (*endptr != '\0') {
+				fprintf(stderr, "Failed to parse code at pos %d.\n", arg - 1);
+				return 1;
+			}
+
+			if (hex > 0xFFul) {
+				fprintf(stderr, "Too large code val at pos %d.\n", arg - 1);
+				fprintf(stderr, "Every val must be a byte.\n");
+				return 2;
+			}
+
+			code[arg - 1] = (uint8_t)hex;
+		}
 	}
 
 	fd = open(DEVICE_FILE_NAME, O_RDWR);
 	if (fd < 0) {
-		fprintf(stderr, "Failed to open file.\n");
-		close(fd);
+		fprintf(stderr, "%s: Failed to open the device file.\n", DEVICE_FILE_NAME);
 		return 3;
 	}
 
 	printf("Will run the following code: ");
-	for (arg = 0; arg < argc - 1; arg++) {
+	for (arg = 0; arg < size; arg++) {
 		printf("%X ", code[arg]);
 	}
 	printf("\n");
 
-	if (sandbox(fd, code, argc - 1)) {
+	if (sandbox(fd, code, size)) {
 		fprintf(stderr, "Failed to run code in sandbox\n");
 		close(fd);
 		return 4;
